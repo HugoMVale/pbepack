ada89c8d05fe65edacda5155ba39638ca8fa1982
HugoMVale <57530119+HugoMVale@users.noreply.github.com>
2022-09-26 22:38:16 +0200

    add ftlListDouble

--- src/ftlList.F90
+++ src/ftlList.F90
@@ -1,11 +1,19 @@
-! Instantiate the module from the ftlList template
+! Instantiate 'ftlListIntModule' from the ftlList template
 #define FTL_TEMPLATE_TYPE integer
 #define FTL_TEMPLATE_TYPE_NAME Int
 #define FTL_INSTANTIATE_TEMPLATE
 #include "ftlList.F90_template"
 
-! Make fpm recognize the instantiated module(needs to be fixed in fpm)
+! Instantiate 'ftlListDoubleModule' from the ftlList template
+#define FTL_TEMPLATE_TYPE real(kind=8)
+#define FTL_TEMPLATE_TYPE_NAME Double
+#define FTL_INSTANTIATE_TEMPLATE
+#include "ftlList.F90_template"
+
+! Make fpm recognize the instantiated modules (needs to be fixed in fpm)
 #if 0
 module ftlListIntModule
 end module ftlListIntModule
+module ftlListDoubleModule
+end module ftlListDoubleModule
 #endif
