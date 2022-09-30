5349be94e55f91a83186a22f74e792b76c1bb309
HugoMVale <57530119+HugoMVale@users.noreply.github.com>
2022-09-30 23:42:55 +0200

    start test for constant agg kernel

--- test/test_agg1d.f90
+++ test/test_agg1d.f90
@@ -21,7 +21,8 @@ contains
       type(unittest_type), allocatable, intent(out) :: testsuite(:)
 
       testsuite = [ &
-                  new_unittest("mass conservation", test_mass_conservation) &
+                  new_unittest("mass conservation", test_mass_conservation), &
+                  new_unittest("analytical solution, case 1", test_case1) &
                   !new_unittest("calc_c", test_calc_c), &
                   !new_unittest("wenok with non-uniform grid", test_wenok_nonuniform) &
                   ]
@@ -31,7 +32,7 @@ contains
    subroutine test_mass_conservation(error)
       type(error_type), allocatable, intent(out) :: error
 
-      integer, parameter :: nc = 123
+      integer, parameter :: nc = 120
       type(grid1) :: gx
       type(combarray) :: aggcomb(nc)
       real(rk), dimension(nc) :: np, birth, death
@@ -69,83 +70,31 @@ contains
 
    end subroutine test_mass_conservation
 
-   ! subroutine test_mass_conservation(error)
-   !    type(error_type), allocatable, intent(out) :: error
+   subroutine test_case1(error)
+      type(error_type), allocatable, intent(out) :: error
 
-   !    integer, parameter :: nc = 100
-   !    type(grid1) :: gx
-   !    type(combarray) :: aggcomb(nc)
-   !    real(rk), dimension(nc) :: np, birth, death
-   !    real(rk) :: t, y(0:0), t0, tend
-   !    integer :: i, m, scl
+      integer, parameter :: nc = 100
+      type(grid1) :: gx
+      type(combarray) :: aggcomb(nc)
+      real(rk), dimension(nc) :: n, birth, death
+      real(rk) :: t, y(0:0), t0, tend
+      integer :: i, m, scl
 
-   !    do scl = 1, 2
-   !       call gx%new(0._rk, 1e3_rk, nc, scl=scl)
-   !       do m = 1, 3
-   !          call agg_init(gx, 1, aggcomb)
-   !          np = 0
-   !          np(1:nc/2 - 1) = 1
-   !          t = 0._rk
-   !          y = 0
-   !          call agg(np, gx, aggcomb, aconst, t, y, birth, death)
-   !          print *, "dn/n0=", sum(birth - death)/sum(np)
-   !          print *, "birth=", sum(birth*gx%center)
-   !          print *, "death=", sum(death*gx%center)
-   !       end do
-   !    end do
-   !    ! do i = 1, nc
-   !    !    print *, "i= ", i
-   !    !    print *, "ia=", aggcomb(i)%ia
-   !    !    print *, "ib=", aggcomb(i)%ib
-   !    !    print *, "we=", aggcomb(i)%weight
-   !    ! end do
+      ! ! System parameters
+      ! n0 = 1._rk
+      ! x0 = 1._rk
+      ! a0 = 1._rk
 
-   !    call cpu_time(t0)
-   !    call cpu_time(tend)
+      ! ! Grid
+      ! call gx%new(0._rk, 10*x0, nc, scl=1)
 
-   !    print '("Time = ",f8.5," seconds.")', (tend - t0)
+      ! ! Initial condition
+      ! n = cell_average(expo1d, gx)
+      ! time = 0
 
-   !    !call agg1(np, gx, aggcomb, afun, t, y, birth, death)
+      ! Call solver
 
-   !    ! call check(error, 1, 1)
-   !    ! real(rk), dimension(:), allocatable :: vext, vl, vr
-   !    ! real(rk) :: eps, atol
-   !    ! integer :: i, k, nc = 30
-
-   !    ! ! Run check for each order
-   !    ! eps = 1e-6_rk
-   !    ! atol = 1e-9_rk
-   !    ! do k = 1, 3
-
-   !    !    ! Allocate arrays
-   !    !    if (allocated(vext)) deallocate (vext)
-   !    !    if (allocated(vl)) deallocate (vl)
-   !    !    if (allocated(vr)) deallocate (vr)
-   !    !    allocate (vext(1 - (k - 1):nc + (k - 1)), vl(nc), vr(nc))
-
-   !    !    ! Set cell average value including ghost cells
-   !    !    ! Just a reactangular pulse __|¯¯|__
-   !    !    vext = 0
-   !    !    vext(nc/3:2*nc/3) = 1
-
-   !    !    ! Call procedure
-   !    !    call wenok(k, eps, vext, vl, vr)
-
-   !    !    ! Check error
-   !    !    call check(error, vext(1:nc), vl, thr=atol)
-   !    !    call check(error, vext(1:nc), vr, thr=atol)
-
-   !    !    ! Detailed comparison for debugging
-   !    !    if (allocated(error) .or. verbose) then
-   !    !       write (stderr, '(2(a4),3(a26))'), "k", "i", "v(i)", "vl(i)", "vr(i)"
-   !    !       do i = 1, nc
-   !    !          write (stderr, '(2(i4),3(es26.16e3))'), k, i, vext(i), vl(i), vr(i)
-   !    !       end do
-   !    !    end if
-
-   !    ! end do
-
-   ! end subroutine test_1
+   end subroutine test_case1
 
    pure real(rk) function aconst(xa, xb, t, y) result(res)
    !! Constant aggregation kernel for 1D system
@@ -157,7 +106,69 @@ contains
          !! time
       real(rk), intent(in) :: y(:)
          !! environment vector
-      res = 1._rk
+      real(rk), parameter :: a0 = 1._rk
+      res = a0
    end function
+
+   pure real(rk) function asum(xa, xb, t, y) result(res)
+   !! Sum aggregation kernel for 1D system
+      real(rk), intent(in) :: xa
+         !! internal coordinate of particle a
+      real(rk), intent(in) :: xb
+         !! internal coordinate of particle b
+      real(rk), intent(in) :: t
+         !! time
+      real(rk), intent(in) :: y(:)
+         !! environment vector
+      real(rk), parameter :: a1 = 1._rk
+      res = a1*(xa + xb)
+   end function
+
+   pure real(rk) function aprod(xa, xb, t, y) result(res)
+   !! Product aggregation kernel for 1D system
+      real(rk), intent(in) :: xa
+         !! internal coordinate of particle a
+      real(rk), intent(in) :: xb
+         !! internal coordinate of particle b
+      real(rk), intent(in) :: t
+         !! time
+      real(rk), intent(in) :: y(:)
+         !! environment vector
+      real(rk), parameter :: a1 = 1._rk
+      res = a1*xa*xb
+   end function
+
+   elemental real(rk) function expo1d(x, x0, n0)
+   !! 1D Exponential distribution.
+      real(rk), intent(in) :: x
+         !! random variable
+      real(rk), intent(in) :: x0
+         !! mean value
+      real(rk), intent(in) :: n0
+         !! initial number of particles
+
+      expo1d = (n0/x0)*exp(-x/x0)
+
+   end function expo1d
+
+   elemental real(rk) function solution_case1(x, x0, n0, a0, t) result(res)
+   !! Anylytical solution for IC='expo1d' and constant aggregation frequency.
+      real(rk), intent(in) :: x
+         !! random variable
+      real(rk), intent(in) :: x0
+         !! mean value
+      real(rk), intent(in) :: n0
+         !! initial number of particles
+      real(rk), intent(in) :: a0
+         !! aggregation frequency
+      real(rk), intent(in) :: t
+         !! time
+
+      real(rk) :: tau
+
+      tau = n0*a0*t
+      res = 4*n0/(x0*(tau + 2._rk)**2)*exp(-2*(x/x0)/(tau + 2._rk))
+
+   end function solution_case1
 
 end module test_agg1d
