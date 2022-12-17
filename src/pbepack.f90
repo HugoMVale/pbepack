501ae1fe3d52302d488bca7830b50de56555d48d
HugoMVale <57530119+HugoMVale@users.noreply.github.com>
2022-12-17 20:31:11 +0100

    refactor api

--- src/pbepack.f90
+++ src/pbepack.f90
@@ -1,10 +1,8 @@
 module pbepack
    use pbepack_pbe1, only: pbe1
-   use pbepack_agg1, only: aggterm
-   use pbepack_break1, only: breakterm
    implicit none
    private
 
-   public :: pbe1, aggterm, breakterm
+   public :: pbe1
 
 end module pbepack
