ca652424c98cdcff9395ee24261f304f2d0440a4
HugoMVale <57530119+HugoMVale@users.noreply.github.com>
2022-12-19 19:09:30 +0100

    add boxcar

--- src/pbepack_math.f90
+++ src/pbepack_math.f90
@@ -16,4 +16,18 @@ contains
 
    end function delta_kronecker
 
+   elemental real(rk) function boxcar(x, a, b, c) result(res)
+   !! Boxcar function.
+      real(rk), intent(in) :: x
+         !! argument
+      real(rk), intent(in) :: a, b
+         !! interval limit \( a < b \)
+      real(rk), intent(in) :: c
+         !! pulse height
+
+      res = ZERO
+      if (x > a .and. x < b) res = c
+
+   end function boxcar
+
 end module pbepack_math
