1568c4bab26de6fea4acc730b9abe44a8ef0c534
HugoMVale <57530119+HugoMVale@users.noreply.github.com>
2022-12-19 15:29:19 +0100

    add dp

--- src/pbepack_kinds.F90
+++ src/pbepack_kinds.F90
@@ -1,20 +1,19 @@
 module pbepack_kinds
 !! Aux module to define real precision.
-   use, intrinsic :: iso_fortran_env, only: real32, real64, real128
+   use, intrinsic :: iso_fortran_env, only: real32, real64
    implicit none
    private
 
-   public :: rk, ONE, ZERO, TWO, HALF
+   public :: rk, dp, ONE, ZERO, TWO, HALF
 
 ! #ifdef REAL32
 !    integer, parameter :: rk = real32
 ! #elif REAL64
    integer, parameter :: rk = real64
-! #elif REAL128
-!    integer, parameter :: rk = real128
 ! #else
 !    integer, parameter :: rk = real64
 ! #endif
+   integer, parameter :: dp = real64
    real(rk), parameter :: ZERO = 0._rk, ONE = 1._rk, TWO = 2._rk
    real(rk), parameter :: HALF = ONE/2
 
